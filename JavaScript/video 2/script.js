1198ab50f7e6c849ebb43e96ec00116719300e2c
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-09-07 22:04:26 +0500

    information about variables, data types, typeof function and basic info about objects in js

---
+++ JavaScript/video 2/script.js
@@ -0,0 +1,43 @@
+console.log("Hey this is tutorial 2 of javascript");
+
+let a = 5;
+a = a + 2;
+let b = 6;
+let c = "bilal";
+
+// console.log(a + b + 8);
+// console.log(typeof a, typeof b, typeof c);
+
+{
+    // let a = 66;
+    console.log(a);
+}
+
+console.log(a);
+
+// const a1 = 11;
+// a1 = a1 + 2;
+// NOT ALLOWED to reassign const
+
+
+let x = "salman bhai";
+let y = 22;
+let z = 33.2;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    name: "Bilal",
+    "job code": 5600,
+    is_handsome: true
+};
+
+console.log(o);
+o.salary = "90K rupees";
+console.log(o);
+o.salary = "200K rupees";
+console.log(o);
