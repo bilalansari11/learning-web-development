bb9697d314f3bbf64266cf17478444e52ec69956
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-09-09 15:44:09 +0500

    information about operators & conditional statements

---
+++ JavaScript/video 3/index.js
@@ -0,0 +1,65 @@
+console.log("This is tutorial 3");
+
+// let age = 3;
+// let grace = 2;
+
+// Arithmetic Operators
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace); // "**" means exponentials or power
+// console.log(age % grace);
+
+// if ((age + grace) > 18) {
+//     console.log("You can drive.")
+// } else {
+//     console.log("You cannot drive.")
+// }
+
+// Assignment Operators
+
+// age += grace; // here "age + grace = 5"
+// age -= grace; // here "age - grace = 1"
+// age *= grace; // here "age * grace = 6"
+// age /= grace; // here "age / grace = 1.5"
+// age %= grace; // here "age % grace = 1"
+// age **= grace; // here "age ** grace = 9"
+
+// console.log(age);
+
+// let age = 1;
+
+// // Comparison Operators
+
+// if (age == 18) {
+//     console.log("You can drive.");
+// }
+// else if(age == 0) {
+//     console.log("Are you kidding!");
+// }
+// else if(age == 1) {
+//     console.log("Are you again kidding!");
+// }
+//  else {
+//     console.log("You cannot drive.");
+// }
+
+// Ternary Operator
+
+let a = 6;
+let b = 8;
+
+c = a > b ? (a - b) : (b - a); //ternary operator
+
+console.log(c);
+
+/* above ternary operator translates
+    if (a>b) {
+    let c = a - b;
+    }
+    else {
+        let c = b - a;
+    }
+*/
