113d25787e46bb9698bbe058cc9fa74392e28e36
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-09-12 12:55:05 +0500

    information about loops in JS

---
+++ JavaScript/video 4/index.js
@@ -0,0 +1,40 @@
+console.log("I am tutorial on loops");
+
+// let a = 1;
+
+// console.log(a);
+// console.log(a + 1);
+// console.log(a + 2);
+// console.log(a + 3);
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Bilal",
+//     role: "Student",
+//     institute: "MAJU"
+// }
+
+// for (const key in obj) {
+
+//     // const element = obj[key];
+//     console.log(key)
+// }
+
+// for (const c of "Bilal") {
+//     console.log(c);
+// }
+
+// let i = 7; 
+// while (i < 6) {
+//     console.log(i);   // there will be no output b/c condition is false
+//     i++;
+// }
+
+let i = 7;
+do {
+    console.log(i);  // there will print 7 than it will check condition
+    i++
+} while (i < 6);
